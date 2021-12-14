c5dffbc3d36fec5432df6e81dea8cd491670a4ca
mai <maitsuji@g.ecc.u-tokyo.ac.jp>
2021-12-14 17:40:44 +0900

    can_promote変更

--- game/src/Referee.c
+++ game/src/Referee.c
@@ -105,20 +105,25 @@ static bool is_legal_drop(Referee this, Board board, Piece piece, Point dest){
 
 }
 
-static bool can_promote(Board this, Piece piece, Point dest, Move move){
-    return false;
+static bool can_promote(Board this, Piece piece, Point dest){
+    //return false;
     //成れるかどうか判定する
-    if (move.will_promote){//dest[2] == "N";最後がN
-        if (get_location(piece).y >= 0 && get_location(piece).y <= 4){//直接指してない
-            if((dest.y >= 0 && dest.y <= 1) || (get_location(piece).y >= 0 && get_location(piece).y <= 1)) { //4A-5Aに移動||4A-5Aから移動
-                return true; //成る
+    //if (move.will_promote){//dest[2] == "N";最後がN
+        //if (get_location(piece).y >= 0 && get_location(piece).y <= 4){//直接指してない
+    if (piece->get_side(piece) == FIRST){ //PieceがFistなら
+        if((dest.y == 0 || (piece->get_location(piece).y == 0)) { //敵陣に移動or敵陣から移動
+            return true; //成る
         }
+    }
+    if (piece->get_side(piece) == SECOND){ //PieceがFistなら
+        if((dest.y == 4 || (piece->get_location(piece).y == 4)) { //敵陣に移動or敵陣から移動
+            return true; //成る
+        }
+    }
+
+
         else
             return false; //反則;敗北
-
-    }
-    
-}
 
 }
 
